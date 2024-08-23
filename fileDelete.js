9faeca38a72526452ce3551707eb095d37bdfd88
avishshiroya <avishshiroya.crawlapps@gmail.com>
2024-08-23 11:48:40 +0530

    feat : create the fileuplod with type validation function

---
+++ fileDelete.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+const document= [".doc",".pdf",".docx",".png",".jpeg",".jpg",".mov"]
+const userProfile = [".doc",".docx",".pdf",".png"]
+
+const fileUpload = async(file,filepath,type)=>{
+        let fileType = path.extname(file.name).toLowerCase();
+        var isUpload ;
+        if(type == "document"){
+            isUpload = document.includes(fileType)
+        }else if(type == "userProfile"){
+            isUpload = userProfile.includes(fileType)
+        } else if(type == "profileImage"){
+            isUpload = true;
+        }
+        
+        if(isUpload){
+            let docName = Date.now() + "_" + Math.round(Math.random() * 1e9) + path.extname(file.name);
+            await file.mv(filepath + docName);
+            let filePath = filepath + docName;
+            return filePath;
+        }else{
+            return null
+        }
+}
+
+module.exports = fileUpload
