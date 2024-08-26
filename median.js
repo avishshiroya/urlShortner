9fac2b0b637916af708cfbba7eaaaa2607c2af7d
avishshiroya <avishshiroya.crawlapps@gmail.com>
2024-08-26 17:33:02 +0530

    feat : practice of patterns

--- median.js
+++ median.js
@@ -26,4 +26,239 @@ const secondLarge = (arr) => {
     }
 }
 
-console.log(secondLarge([1, 45, 75, 87, 95, 75, 1]));
+// console.log(secondLarge([1, 45, 75, 87, 95, 75, 1]));
+
+
+// Rotate the array with K Element
+
+const reverse = (arr, to, from) => {
+    while (to < from) {
+        let temp = arr[to];
+        arr[to++] = arr[from];
+        arr[from--] = temp;
+    }
+}
+const rotateArrayByK = (arr, k) => {
+    if (arr.length > k) {
+        k = k % arr.length;
+    } else {
+        return arr
+    }
+
+    reverse(arr, 0, arr.length - 1);
+    // console.log(arr);
+    reverse(arr, 0, k - 1);
+    // console.log(arr);
+    reverse(arr, k, arr.length - 1);
+    // console.log(arr);
+    return arr;
+}
+// console.log(rotateArrayByK([1, 2, 3, 4, 5, 6, 7], 6));
+
+// Remove Duplicates from Sorted Array
+// Given an integer array nums sorted in non-decreasing order, remove
+// the duplicates in-place such that each unique element appears
+// only once.The relative order of the elements should be kept
+// the same.Then return the number of unique elements in nums.
+
+// Input: [1,1,2]               ----->>>>>  Output: 2, [1,2,_]
+// Input: [0,0,1,1,1,2,2,3,3,4] ----->>>>>  Output: 5, [0,1,2,3,4,_,_,_,_,_]
+
+
+const getUnique = (arr) => {
+    if (arr.length === 0) return 0;
+    let count = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[count] !== arr[i]) {
+            count++;
+            arr[count] = arr[i];
+        }
+    }
+    return { count: count + 1, arr };
+}
+
+console.log(getUnique([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
+
+// for (let i = 0; i < 9; i++) {
+//     let row = "";
+//     for (let j = 1; j <= 9 - i; j++) {
+//         row += " ";
+//     }
+//     for (let p = 1; p <= i; p++) {
+//         row += "*" + " ";
+//     }
+//     console.log(row);
+// }
+// output:-
+//         * 
+//        * * 
+//       * * * 
+//      * * * * 
+//     * * * * * 
+//    * * * * * * 
+//   * * * * * * * 
+//  * * * * * * * * 
+
+//========        chnage this line: -
+// for (let i = 9; i > 0 ; i--) {
+
+
+// * * * * * * * * * 
+//  * * * * * * * * 
+//   * * * * * * * 
+//    * * * * * * 
+//     * * * * * 
+//      * * * * 
+//       * * * 
+//        * * 
+//         * 
+
+
+
+
+// var moveZeroes = function (nums) {
+//     let i = 0;
+//     for (let j = 0; j < nums.length; j++) {
+//         if (nums[j] !== 0) {
+//             let temp = nums[i];
+//             nums[i++] = nums[j]
+//             nums[j] = temp
+//         }
+//     }
+//     return nums
+// };
+
+// console.log(moveZeroes([0, 0, 1]));
+
+
+
+const pattern2 = (rows, variable) => {
+    let pattern = "";
+    let start = variable
+    // let start = variable.charCodeAt(0)
+    for (let i = 1; i <= rows; i++) {
+        for (let j = 1; j <= i; j++) {
+            pattern += start + " ";
+            // pattern += String.fromCharCode(start) + " ";
+            start++
+        }
+        console.log(pattern);
+        pattern = ""
+    }
+}
+// pattern2(5, 4);
+
+// 4 
+// 5 6 
+// 7 8 9 
+// 10 11 12 13 
+// 14 15 16 17 18 
+
+
+//after open commented code and comment above line
+// pattern2(5, "A");
+// A 
+// B C 
+// D E F 
+// G H I J 
+// K L M N O 
+
+// let count = 1
+// for (let i = 0; i <= 3; i++) {
+//     let row = "";
+//     for (let j = 0; j < 9 - i; j++) {
+//         row += " "
+//     }
+//     for (let k = 0; k < i; k++) {
+//         row += count++ + " ";
+//     }
+//     console.log(row);
+// }
+//     1
+//    2 3
+//   4 5 6 
+
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+//     for (let j = 1; j <= 5; j++) {
+//         if (i === 1 || i === 5) row += "*"
+//         else if (j === 1 || j === 5) row += "*"
+//         else row += " "
+//     }
+//     console.log(row);
+// }
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+// for (let i = 0; i < 5; i++) {
+//     let row = "";
+//     for (let j = 0; j < 5 - i; j++) {
+//         row += " "
+//     }
+//     for (let k = 0; k < i + 1; k++) {
+//         row += "*"
+//     }
+//     console.log(row);
+// }
+
+//      *
+//     **
+//    ***
+//   ****
+//  *****
+
+//when swap the for loops;
+
+// *     
+// **    
+// ***   
+// ****  
+// ***** 
+
+// for (let i = 0; i < 10; i++) {
+//     let row = "";
+//     for (let j = 0; j < i + 1; j++) {
+//         if (j === 0 || j === i || i === 9) {
+//             row += "*"
+//         } else {
+//             row += " "
+//         }
+//     }
+//     console.log(row);
+// }
+
+// *
+// **
+// * *
+// *  *
+// *   *
+// *    *
+// *     *
+// *      *
+// *       *
+// **********
+
+// if (10 % 2 === 0) {
+//     const mid = 10 / 2;
+//     for (let i = 0; i < mid; i++) {
+//         console.log("*".repeat(i + 1));
+//     }
+//     for (let j = mid; j > 0; j--) {
+//         console.log("*".repeat(j - 1));
+//     }
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
